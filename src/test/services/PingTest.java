e454d0834b33a85aae7861d66da81018de89e522
Nika Chkhartishvili <chxartishvilin@gmail.com>
2016-06-11 17:56:47 +0400

    Add Ping test

---
+++ src/test/services/PingTest.java
@@ -0,0 +1,30 @@
+package services;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.junit.Test;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nika on 6/11/16.
+ */
+public class PingTest extends JerseyTest {
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(Ping.class);
+    }
+
+    @Test
+    public void testGet() throws Exception {
+
+        Response resp = target("ping").request().get();
+        assertEquals(200, resp.getStatus());
+
+    }
+
+}
