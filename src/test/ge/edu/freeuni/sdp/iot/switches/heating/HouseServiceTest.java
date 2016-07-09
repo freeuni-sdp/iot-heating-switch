3757ac6df853492c1513ea3b5da35818e118b85d
Nika Chkhartishvili <chxartishvilin@gmail.com>
2016-07-09 23:50:19 +0400

    Implement House service GET test

---
+++ src/test/ge/edu/freeuni/sdp/iot/switches/heating/HouseServiceTest.java
@@ -0,0 +1,71 @@
+package ge.edu.freeuni.sdp.iot.switches.heating;
+
+import ge.edu.freeuni.sdp.iot.switches.heating.core.HouseRegistry;
+import ge.edu.freeuni.sdp.iot.switches.heating.core.HouseRegistryFactory;
+import ge.edu.freeuni.sdp.iot.switches.heating.model.House;
+import ge.edu.freeuni.sdp.iot.switches.heating.model.Switch;
+import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.test.JerseyTest;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by nika on 7/9/16.
+ */
+public class HouseServiceTest extends JerseyTest {
+
+    @Mock private HouseRegistry registry;
+    private String houseId;
+
+    @Override
+    protected Application configure() {
+        return new ResourceConfig(HouseService.class);
+    }
+
+    @Before
+    public void setUpChild() throws Exception {
+        houseId = "3c5afb74-2e82-4f10-9931-89187fe47adf";
+
+        MockitoAnnotations.initMocks(this);
+        HouseRegistryFactory.setTestEntry(registry);
+        HouseRegistryFactory.setTestMode(true);
+    }
+
+    @After
+    public void tearDownChild() throws Exception {
+        HouseRegistryFactory.setTestMode(false);
+    }
+
+    @Test
+    public void get() throws Exception {
+        House house = new House(houseId);
+        house.add(new Switch("1", true).setAvailable(true));
+        house.add(new Switch("2", false).setAvailable(true));
+        house.add(new Switch("3", true).setAvailable(false));
+        house.add(new Switch("4", false).setAvailable(false));
+        when(registry.getHouse(houseId)).thenReturn(house);
+
+        Response response = target("/house/" + houseId).request().get();
+
+        assertEquals(200, response.getStatus());
+
+        JSONObject respObject = new JSONObject(response.readEntity(String.class));
+        House respHouse = House.fromJson(respObject);
+
+        assertEquals(house, respHouse);
+    }
+
+
+
+}
